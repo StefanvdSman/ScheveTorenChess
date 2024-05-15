84a903183118ac0391093f089fc7bc5393c70e86
StefanvdSman <sj.vandersman@gmail.com>
2024-05-15 04:53:39 +0200

    add logo and visual components for search and darkmode

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,55 @@
-import React from 'react'
-
+import { Button, Navbar,TextInput } from 'flowbite-react'
+import { Link, useLocation } from 'react-router-dom'
+import {AiOutlineSearch} from 'react-icons/ai'
+import {FaMoon} from 'react-icons/fa'
 export default function Header() {
+    const path = useLocation().pathname;
   return (
-    <div>Header</div>
+    <Navbar className='border-b-2'>
+        <Link
+         to='/'
+         className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'>
+            Schaakvereniging
+            <span className='px-2 py-1 bg-gradient-to-r from-cyan-500 to-blue-500 rounded-lg text-white'>De Scheve Toren</span>
+        </Link>
+        <form>
+            <TextInput
+                type ='text'
+                placeholder='Search...'
+                rightIcon={AiOutlineSearch}
+                className='hidden lg:inline'
+            />
+        </form>
+        <Button className='w-12 h-10 lg:hidden rounded-full' color='gray' >
+            <AiOutlineSearch />
+        </Button>
+        <div className='flex gap-2 md:order-2'>
+            <Button className='w-12 h-10 hidden sm:inline rounded-full' color='gray'>
+                <FaMoon />
+            </Button>
+            <Link to='/signin'>
+                <Button  gradientDuoTone='purpleToBlue'>
+                    Sign In
+                </Button>
+            </Link>
+            <Navbar.Collapse>
+                <Navbar.Link active={path === '/'} as={'div'}>
+                    <Link to='/'>
+                        Home
+                    </Link>
+                </Navbar.Link>
+                <Navbar.Link active={path === '/about'} as={'div'}>
+                    <Link to='/about'> 
+                        About
+                    </Link>
+               </Navbar.Link>
+                <Navbar.Link active={path === '/projects'} as={'div'}>
+                    <Link to='/projects'>
+                        Projects
+                    </Link>
+                </Navbar.Link>
+            </Navbar.Collapse>
+        </div>
+    </Navbar>
   )
 }
