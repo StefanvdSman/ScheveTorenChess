cb3bc660088b6db9df24a4c13e37e355a3375fdf
StefanvdSman <sj.vandersman@gmail.com>
2024-05-20 19:08:05 +0200

    add password encryption

--- server/database/models/user.model.js
+++ server/database/models/user.model.js
@@ -1,7 +1,10 @@
 import { Timestamp } from 'mongodb';
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
 
-const userSchema = new mongoose.Schema(
+SALT_WORK_FACTOR = 10;
+
+let userSchema = new mongoose.Schema(
   {
     username: {
       type: String,
@@ -21,6 +24,25 @@ const userSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
+userSchema.pre('save', async function (next) {
+  let user = this;
+  if (!user.isModified('password')) return next();
+  bcrypt.genSalt(SALT_WORK_FACTOR, function (err, salt) {
+    if (err) return next(err);
+    bcrypt.hash(user.password, salt, function (err, hash) {
+      if (err) return next(err);
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+userSchema.methods.comparePassword = async function (candidatePassword, cb) {
+  bcrypt.compare(candidatePassword, this.password, function (err, isMatch) {
+    if (err) return cb(err);
+    cb(null, isMatch);
+  });
+};
 const User = mongoose.model('User', userSchema);
 
 export default User;
