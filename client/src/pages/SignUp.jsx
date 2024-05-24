6f150bc5c1200d3fc7470f1f856cc8aeba1b0f47
StefanvdSman <sj.vandersman@gmail.com>
2024-05-24 14:48:04 +0200

    add api proxy and signup logic

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,8 +1,39 @@
 import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react';
 import { Link } from 'react-router-dom';
 import Logo from '../components/Logo';
-
+import { useState } from 'react';
 export default function SignUp() {
+  const [formData, setFormData] = useState({});
+  const [errorMessage, setErrorMessage] = useState('');
+  const [loading, setLoading] = useState(false);
+  const HandleChange = (event) => {
+    setFormData({
+      ...formData,
+      [event.target.id]: event.target.value.trim(),
+    });
+  };
+  const HandleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      setLoading(true);
+      setErrorMessage('');
+      const res = await fetch('api/auth/signup', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      res.json().then((res) => {
+        setErrorMessage(res.message === 'Signup successful' ? '' : res.message);
+        console.log(res);
+        setLoading(false);
+      });
+    } catch (err) {
+      setErrorMessage(err.message);
+      setLoading(false);
+    }
+  };
   return (
     <div className='min-h-screen mt-20'>
       <div className='flex p-3  max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5'>
@@ -16,7 +47,12 @@ export default function SignUp() {
           <form>
             <div>
               <Label value='Your username' />
-              <TextInput placeholder='Username' id='username' type='text' />
+              <TextInput
+                placeholder='Username'
+                id='username'
+                type='text'
+                onChange={HandleChange}
+              />
             </div>
             <div>
               <Label value='Your email' />
@@ -24,15 +60,34 @@ export default function SignUp() {
                 placeholder='name@example.com'
                 id='email'
                 type='text'
+                onChange={HandleChange}
               />
             </div>
             <div>
               <Label value='Your password' />
-              <TextInput placeholder='Password' id='password' type='password' />
+              <TextInput
+                placeholder='Password'
+                id='password'
+                type='password'
+                onChange={HandleChange}
+              />
             </div>
           </form>
-          <Button gradientDuoTone='greenToBlue' type='submit' className='mt-5'>
-            Sign Up
+          <Button
+            gradientDuoTone='greenToBlue'
+            type='submit'
+            className='mt-5'
+            onClick={HandleSubmit}
+            disabled={loading}
+          >
+            {loading ? (
+              <>
+                <Spinner size='sm' />
+                <span className='pl-3'>Loading...</span>
+              </>
+            ) : (
+              'Sign Up'
+            )}
           </Button>
           <div className='flex gap-2 text-sm mt-5'>
             <p>Already have an account?</p>
@@ -40,6 +95,16 @@ export default function SignUp() {
               <p className='text-blue-500'>Sign In</p>
             </Link>
           </div>
+          {errorMessage && (
+            <Alert
+              className='mt-5'
+              color={
+                errorMessage === 'SignUp successful' ? 'success' : 'failure'
+              }
+            >
+              {errorMessage}
+            </Alert>
+          )}
         </div>
       </div>
     </div>
